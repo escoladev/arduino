ef358207eccb1bb006925532faea3bc75ac87341
jhoonb <jpbanczek@gmail.com>
2019-11-15 12:09:10 -0400

    update

---
+++ led_rgb_potenciometro/README.md
@@ -0,0 +1,5 @@
+## LED RGB com Potênciometro
+
+Siga o exemplo do esquema abaixo
+
+- controle cada uma das cores(R,G,B) com um potênciometro.
---
+++ led_rgb_potenciometro/led_rgb_potenciometro.ino
@@ -0,0 +1,71 @@
+/*
+ * LED RGB - cores controlada por Potênciometro
+ * Jhonathan. Banczek
+ * github.com/jhoonb
+ */
+
+#define RED 3
+#define BLUE 5
+#define GREEN 6
+
+const int pinoPOT0 = A0; // pino de entrada do potenciômetro BLUE
+const int pinoPOT1 = A1; // pino de entrada do potenciômetro GREEN
+const int pinoPOT2 = A2; // pino de entrada do potenciômetro RED
+
+int valorRED = 0;
+int valorGREEN = 0;
+int valorBLUE = 0;
+
+void setup() {
+  pinMode(pinoPOT2, INPUT);
+  pinMode(pinoPOT1, INPUT); 
+  pinMode(pinoPOT0, INPUT);
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+/*
+ * limite: função que retorna o valor máximo 255
+ * se o valor recebido pelo potênciometro 
+ * for igual ou maior a 253.
+ * [apenas para limite e teste do potênciometro, depende do resistor]
+ */
+int limite(int vcor) {
+  if(vcor >= 255) {
+    return 255;
+  }
+  return vcor;
+}
+
+
+void loop() {
+  // testar antes se o maximo da 255 [+ 10]
+  valorRED = map(analogRead(pinoPOT2), 0, 1023, 0, 265); 
+  valorGREEN = map(analogRead(pinoPOT1), 0, 1023, 0, 265); //
+  valorBLUE = map(analogRead(pinoPOT0), 0, 1023, 0, 265); //
+
+  // verifica o limite
+  valorRED = limite(valorRED);
+  valorGREEN = limite(valorGREEN);
+  valorBLUE = limite(valorBLUE);
+
+  // exibir no Monitor Serial
+  Serial.print("valorRED: ");
+  Serial.print(valorRED);
+  Serial.print("       valorGREEN: ");
+  Serial.print(valorGREEN);
+  Serial.print("       valorBLUE: ");
+  Serial.println(valorBLUE);
+  
+  // seta os valores no led
+  analogWrite(RED, valorRED);
+  analogWrite(GREEN, valorGREEN);
+  analogWrite(BLUE, valorBLUE);
+  
+  // pode ajustar, quanto menor o valor, 
+  // maior 'precisão' no potênciometro
+  delay(10);
+}
